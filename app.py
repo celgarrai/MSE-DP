0993a60d4b64d28e3d85d7f8d07f7d8711e1ac50
celgarrai <chemseddine123@gmail.com>
2023-11-18 12:12:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,38 +6,11 @@ import tensorflow as tf
 app = Flask(__name__)
 
 # Chargez le modèle
-model = tf.keras.models.load_model('model_classification_images.h5')
+#model = tf.keras.models.load_model('model_classification_images.h5')
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    if 'file' not in request.files:
-        return render_template('index.html', message='Aucun fichier sélectionné')
-
-    file = request.files['file']
-
-    if file.filename == '':
-        return render_template('index.html', message='Aucun fichier sélectionné')
-
-    if file:
-        # Chargez et affichez l'image téléchargée
-        image = Image.open(file)
-        image_array = np.array(image.resize((150, 150)))
-        img_tensor = np.expand_dims(image_array, axis=0) / 255.0
-
-        # Faites des prédictions
-        prediction = model.predict(img_tensor)
-
-        # Affichez le résultat
-        if prediction[0][0] > 0.5:
-            result = 'Chien'
-        else:
-            result = 'Chat'
-
-        return render_template('result.html', prediction=result, confidence=prediction[0][0])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
